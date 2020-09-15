55ec4518f8c8651778ee813d4c29f62d66cecb68
Theo Dias Mitchell <theomitchell@gmail.com>
2020-09-15 14:05:25 -0400

    2nd Try
    After Creating a Sub-Component artist-items with the list of All Artists.
    It is giving a ERROR at the Server as if the artist is NOT being passed to the view, but still shows the APP.

--- src/app/artist-items/artist-items.component.ts
+++ src/app/artist-items/artist-items.component.ts
@@ -3,14 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-artist-items',
   templateUrl: './artist-items.component.html',
-  styleUrls: ['./artist-items.component.css']
-
+  styleUrls: ['./artist-items.component.css'],
+  inputs: ['artist']
 })
 export class ArtistItemsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
