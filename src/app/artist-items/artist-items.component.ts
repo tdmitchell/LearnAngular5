8f243fa16df4b7d18e73a95fe310212af6b77312
Theo Dias Mitchell <theomitchell@gmail.com>
2020-09-15 08:02:04 -0400

    Before separating the Sub-Component artist-item

---
+++ src/app/artist-items/artist-items.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-items',
+  templateUrl: './artist-items.component.html',
+  styleUrls: ['./artist-items.component.css']
+
+})
+export class ArtistItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
