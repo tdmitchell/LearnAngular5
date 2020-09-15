4f89c87ef5f1d45d7c2c01357148fe4ec0ce6443
Theo Dias Mitchell <theomitchell@gmail.com>
2020-09-15 14:06:54 -0400

    Revert "2nd Try"
    This reverts commit 55ec4518f8c8651778ee813d4c29f62d66cecb68.

--- src/app/artist-items/artist-items.component.ts
+++ src/app/artist-items/artist-items.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-artist-items',
   templateUrl: './artist-items.component.html',
-  styleUrls: ['./artist-items.component.css'],
-  inputs: ['artist']
+  styleUrls: ['./artist-items.component.css']
+
 })
 export class ArtistItemsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
+
 }
